e4df4717b1fd7be1ca1700022e2ee1cd9f1fcb22
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2025-01-21 10:55:31 -0600

    update

---
+++ Unidad 1/P01_EjemploBoton.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5 import uic, QtWidgets
+qtCreatorFile = "P01_EjemploBoton.ui"  # Nombre del archivo aquí.
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        # Área de los Signals
+        self.btn_saludar.clicked.connect(self.saludar)
+
+    # Área de los Slots
+    def saludar(self):
+        print("HOLA, buen día!")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
+
