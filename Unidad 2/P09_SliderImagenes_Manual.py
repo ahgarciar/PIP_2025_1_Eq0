5f4aaed22f23c28c3e6164fa189f294dbfca5440
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2025-02-05 13:01:05 -0600

    update

--- Unidad 2/P09_SliderImagenes_Manual.py
+++ Unidad 2/P09_SliderImagenes_Manual.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import uic, QtWidgets, QtGui
-qtCreatorFile = "P08_DescripcionDeImagenes.ui"  # Nombre del archivo aquí.
+qtCreatorFile = "P09_SliderImagenes_Manual.ui"  # Nombre del archivo aquí.
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -9,17 +9,17 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         # Área de los Signals
         self.selectorImagen.setMinimum(1)
-        self.selectorImagen.setMaximum(1)
+        self.selectorImagen.setMaximum(3)
         self.selectorImagen.setSingleStep(1)
         self.selectorImagen.setValue(1) ##valr inicial
         self.selectorImagen.valueChanged.connect(self.cambiaValor)
 
         self.diccionarioDatos = {
-            0: (":/Ejercicios/ImagenGatoEuropeo.png",["Gato", "4 meses", "Raton"]),
-            1: (":/Logos/FIT_logo_vertical.png", ["Castor", "65 años", "Estudiar"]),
-            2: (":/Logos/log_uat_nuevo.png", ["Correcaminos", "75 años", "Superacion"])
+            1: (":/Ejercicios/ImagenGatoEuropeo.png",["Gato", "4 meses", "Raton"]),
+            2: (":/Logos/FIT_logo_vertical.png", ["Castor", "65 años", "Estudiar"]),
+            3: (":/Logos/log_uat_nuevo.png", ["Correcaminos", "75 años", "Superacion"])
         }
-        self.indice = 0
+        self.indice = 1
         self.obtenerDatos()
 
     # Área de los Slots
@@ -31,9 +31,13 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.txt_edad.setText(edad)
         self.txt_juguete.setText(juguete)
 
+        self.imagen_descripcion.setPixmap(QtGui.QPixmap(
+            self.diccionarioDatos[self.indice][0]))
+
     def cambiaValor(self):
-        value = self.selectorImagen.value()
-        #self.txt_valor.selectorImagen(str(value))
+        self.indice = self.selectorImagen.value()
+        self.obtenerDatos()
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
